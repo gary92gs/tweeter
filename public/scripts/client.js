de2c33abea16094030b9ac7ecd659b72a3a89087
gary92gs <gary92.gs@gmail.com>
2024-01-31 09:16:20 -0700

    reformat char-counter js for readability.start work on ajax function in client js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweetObj) {
+  return `<article class="post-tweet-container">   
+      <header>
+        <div class="tweeted-user">
+          <i class="fa-regular fa-user"></i>
+          <span>Username</span>
+        </div>
+        <span>User-Tag</span>
+      </header>
+      <h3>Tweet Contents</h3>
+      <footer class="post-tweet-footer">
+        <span>Post-Age</span>
+        <div>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`;
+};
+
+
+$(document).ready(function() {
+
+});
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,45 +1,15 @@
 $(document).ready(function() {
-
-
   //if user is editing a new tweet
   $('.new-tweet-textarea').on('input', function(event) {
     //check length of string inside textarea and 
     let remainingChars = 140 - this.value.length;
     // update character count element based on the current string length inside textarea
     $(this).parent().find('.counter').html(remainingChars);
-
-    if(remainingChars < 0){
+    //if negative, assign negative class for styling. if positive remove negative class
+    if (remainingChars < 0) {
       $(this).parent().find('.counter').addClass('negative');
     } else {
       $(this).parent().find('.counter').removeClass('negative');
     }
-
-
-
-
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
