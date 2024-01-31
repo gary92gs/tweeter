19c9f6e95432479b95085293cea76bdee8c95fe1
gary92gs <gary92.gs@gmail.com>
2024-01-31 16:32:19 -0700

    add event handler for submitting new tweet. add html element clear for successive tweetDB requests, which would cause duplicates if not cleared. clear is occuring inside renderPostedTweets function. add js post and block html post when new tweet submitted.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,45 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//START TEMP HARD-CODED SECTION (to-be-removed after testing)
-const tweetDatabase = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1706490154190
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1706576554190
-  }
-];
-//END TEMP HARD-CODED SECTION
-
-
 //note: asynchronous request to database (ajax), then call this function to generate html (because we are returning)
 const createTweetElement = function(tweetObj) {
-  //determine post age in days
-  const postDate = new Date(tweetObj.created_at);
-  const currentDate = new Date();
-  const yearsDiff = currentDate.getFullYear() - postDate.getFullYear();
-  let postAge = currentDate.getDay() - postDate.getDay();
-  if(yearsDiff > 0){
-    postAge += (365 * yearsDiff);
-  }
-
+  //determine post age in days with timeago library (imported in index.html header)
+  const postAge = timeago.format(new Date(tweetObj.created_at));
   //create html structure for individual tweet
   const tweet = $(
     `<article class="post-tweet">   
@@ -55,7 +20,7 @@ const createTweetElement = function(tweetObj) {
       </header>
       <h3>${tweetObj.content.text}</h3>
       <footer class="post-tweet-footer">
-        <span>${postAge} days ago</span>
+        <span>${postAge}</span>
         <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -69,20 +34,56 @@ const createTweetElement = function(tweetObj) {
 
 //loads pre-existing tweets into DOM. all function calls must exist in document.ready function
 const renderPostedTweets = function(tweetDBarr) {
+
+
+  //clear DOM before rendering current database
+  $('.post-tweet-container').empty();
+  //append each tweet at the top of the list (prepend)
   for (const tweet of tweetDBarr) {
-    $('.post-tweet-container').append(createTweetElement(tweet));
+    $('.post-tweet-container').prepend(createTweetElement(tweet));
   }
 };
 
-//for posting new tweets (undecided if it will handle dom only or also push post to database)
-const postNewTweet = function() { 
-
+//gets current tweet database
+const loadTweetDB = function() {
+  //request tweets from server database
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    //wait for data, then render all tweets based on data from server database
+    success: (tweetDatabase) => {
+      renderPostedTweets(tweetDatabase);
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
 };
 
 
 $(document).ready(function() {
-  renderPostedTweets(tweetDatabase);
 
+  loadTweetDB();
+
+  // listen for new form submissions
+  $('.new-tweet-container form').on('submit', (event) => {
+    //stop browser from redirecting automatically
+    event.preventDefault();
+    //encode form data submission in urlencoding
+    const data = $('.new-tweet-container form').serialize();
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      // encode post request to urlencoding
+      data: data,
+      success: (response) => {
+        loadTweetDB();
+      },
+      error: (error) => { 
+        console.log(error);
+      },
+    });
+  });
 
 
 });
